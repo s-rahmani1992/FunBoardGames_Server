0e12f100058fb39c96dc17799910967b0e7079f3
Sina Rahmani <rahmani.sina1992@gmail.com>
2026-09-15 23:19:55 +0100

    Added the new migration for SET card attributes.

--- FunBoardGames.Database/Migrations/20260915213238_SET_Game_Attribute.Designer.cs
+++ FunBoardGames.Database/Migrations/20260915213238_SET_Game_Attribute.Designer.cs
@@ -4,6 +4,7 @@ using FunBoardGames.Database;
 using FunBoardGames.Network.SignalR.Shared;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FunBoardGames.Database.Migrations
 {
     [DbContext(typeof(FunBoardGamesDbContext))]
-    partial class FunBoardGamesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260915213238_SET_Game_Attribute")]
+    partial class SET_Game_Attribute
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FunBoardGames.Database/Migrations/20260915213238_SET_Game_Attribute.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FunBoardGames.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class SET_Game_Attribute : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "color_attribute",
+                table: "SET_Games",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "count_attribute",
+                table: "SET_Games",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "shading_attribute",
+                table: "SET_Games",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "shape_attribute",
+                table: "SET_Games",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_SET_Games_color_attribute",
+                table: "SET_Games",
+                sql: "color_attribute BETWEEN 0 AND 2");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_SET_Games_count_attribute",
+                table: "SET_Games",
+                sql: "count_attribute BETWEEN 0 AND 2");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_SET_Games_shading_attribute",
+                table: "SET_Games",
+                sql: "shading_attribute BETWEEN 0 AND 2");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_SET_Games_shape_attribute",
+                table: "SET_Games",
+                sql: "shape_attribute BETWEEN 0 AND 2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_SET_Games_color_attribute",
+                table: "SET_Games");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_SET_Games_count_attribute",
+                table: "SET_Games");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_SET_Games_shading_attribute",
+                table: "SET_Games");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_SET_Games_shape_attribute",
+                table: "SET_Games");
+
+            migrationBuilder.DropColumn(
+                name: "color_attribute",
+                table: "SET_Games");
+
+            migrationBuilder.DropColumn(
+                name: "count_attribute",
+                table: "SET_Games");
+
+            migrationBuilder.DropColumn(
+                name: "shading_attribute",
+                table: "SET_Games");
+
+            migrationBuilder.DropColumn(
+                name: "shape_attribute",
+                table: "SET_Games");
+        }
+    }
+}
